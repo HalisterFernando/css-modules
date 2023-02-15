13c7fe5ceef9a5c67a2cc406e3318d7324c2959f
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-02-15 15:57:57 -0300

    definido elementos do card

--- src/card.js
+++ src/card.js
@@ -2,12 +2,28 @@ import h from 'hyperscript';
 import helpers from 'hyperscript-helpers';
 import { card, cardBody, cardImage } from './styles/card-carousel.css';
 import CatImg from './assets/cat.jpg';
+import Avatar from './assets/avatar.png';
+import Like from './assets/heart.png';
+import Comment from './assets/chat.png';
+import Share from './assets/share.png';
 
-const {div, a, img} = helpers(h);
+const {div, a, img, h3} = helpers(h);
 
-const cat = img({src: CatImg, className: cardImage})
-const body = div({className: cardBody})
+const photo = img({src: CatImg, className: cardImage});
+const avatar = img({src: Avatar, className: cardAvatar});
+const name = h3('Isabela.Souza');
+const like = img({src: Like});
+const comment = img({src: Comment});
+const share = img({src: Share});
+const text = p(' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque exercitationem vel, ad accusantium quod aut sit aliquid.')
+const textContainer = div({classname: textContainer}, text);
 
-const cardContainer = div({className: card}, cat);
+
+const actionIcons = div({className: cardActions}, [like, comment, share])
+
+
+const content = div({className: cardBody}, [avatar, name, actionIcons, textContainer]);
+
+const cardContainer = div({className: card}, [photo, content]);
 
 export default cardContainer
