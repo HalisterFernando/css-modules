ae03f6cbbe52befbb5ccb142d0900f9931c0d24c
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-02-15 16:54:14 -0300

    formatando elementos no card

--- src/card.js
+++ src/card.js
@@ -1,13 +1,13 @@
 import h from 'hyperscript';
 import helpers from 'hyperscript-helpers';
-import { card, cardBody, cardImage } from './styles/card-carousel.css';
+import { card, cardContent, cardImage, cardAvatar, cardActions, cardText } from './styles/card-carousel.css';
 import CatImg from './assets/cat.jpg';
 import Avatar from './assets/avatar.png';
 import Like from './assets/heart.png';
 import Comment from './assets/chat.png';
 import Share from './assets/share.png';
 
-const {div, a, img, h3} = helpers(h);
+const {div, a, img, h3, p} = helpers(h);
 
 const photo = img({src: CatImg, className: cardImage});
 const avatar = img({src: Avatar, className: cardAvatar});
@@ -16,13 +16,13 @@ const like = img({src: Like});
 const comment = img({src: Comment});
 const share = img({src: Share});
 const text = p(' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque exercitationem vel, ad accusantium quod aut sit aliquid.')
-const textContainer = div({classname: textContainer}, text);
+const textContainer = div({classname: cardText}, text);
 
 
 const actionIcons = div({className: cardActions}, [like, comment, share])
 
 
-const content = div({className: cardBody}, [avatar, name, actionIcons, textContainer]);
+const content = div({className: cardContent}, [avatar, name, actionIcons, textContainer]);
 
 const cardContainer = div({className: card}, [photo, content]);
 
