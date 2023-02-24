e894eb49fcc1ef0fc60b159c24e365835c8cc2b6
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-02-24 16:47:23 -0300

    finalizando card video

--- src/CardVideo.js
+++ src/CardVideo.js
@@ -2,16 +2,20 @@ import h from 'hyperscript';
 import helpers from 'hyperscript-helpers';
 import carousel from './styles/card-carousel.css';
 import common from './styles/card-common.css';
-import CatImg from './assets/cat.jpg';
+import Space from './assets/space.png';
 import Avatar from './assets/avatar.png';
 import Like from './assets/heart.png';
 import Comment from './assets/chat.png';
 import Share from './assets/share.png';
 import global from './styles/card-global.css';
+import Camera from './assets/video-camera.png';
 
 const {div, a, img, h3, p} = helpers(h);
 
-const photo = img({src: CatImg, className: common.cardImage});
+const photo = img({src: Space, className: common.cardImage});
+const icon = img({src: Camera})
+const iconContainer = div({className: carousel.cardIcon}, icon);
+const photoContainer = div ({className: common.photoContainer}, [photo, iconContainer])
 const avatar = img({src: Avatar, className: common.cardAvatar});
 const name = h3('Isabela.Souza');
 const user = div({className: common.cardUser}, [avatar, name])
@@ -28,6 +32,6 @@ const textContainer = div({classname: 'cardText'}, text);
 
 const content = div({className: common.cardContent}, [contentHeader, textContainer]);
 
-const cardContainer = div({className: 'card'}, [photo, content]);
+const cardContainer = div({className: 'card'}, [photoContainer, content]);
 
 export default cardContainer
